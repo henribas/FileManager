4b406e3a94242372e6e8f3a0b9a4bf0185825def
henribas <henribas@gmail.com>
2022-09-01 16:14:05 -0300

    Test

--- src/test/java/com/github/henribas/filemanager/webapi/FileManagerWebAPITest.java
+++ src/test/java/com/github/henribas/filemanager/webapi/FileManagerWebAPITest.java
@@ -1,21 +1,31 @@
 package com.github.henribas.filemanager.webapi;
 
-import io.quarkus.test.junit.QuarkusTest;
+import static io.restassured.RestAssured.given;
+
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.builder.MultiPartSpecBuilder;
+import io.restassured.specification.MultiPartSpecification;
 
 @QuarkusTest
-public class FileManagerWebAPITest {
+class FileManagerWebAPITest {
 
-    /*@Test
-    public void testHelloEndpoint() {
+    @Test
+    void testUploadFile() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
-    }*/
+            .multiPart(getMultiPart())
+            .when().post("/api/file-manager/upload")
+            .then()
+                .statusCode(200);
+    }
+
+    private MultiPartSpecification getMultiPart() {
+        return new MultiPartSpecBuilder("Test-Content-In-File".getBytes())
+            .fileName("book.txt")
+            .controlName("file")
+            .mimeType("text/plain")
+            .build();
+    }
 
 }
